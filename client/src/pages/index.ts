33970eb9d4d8b559f1abb4cd129e63082522c83c
StefanKostic88 <stefankostic88@yahoo.com>
2025-03-14 10:35:24 +0100

    routes completed

---
+++ client/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Home } from "./Home/Home";
+export { default as Project } from "./Project/Project";
+export { default as NotFound } from "./NotFound/NotFound";
---
+++ client/src/routes.tsx
@@ -0,0 +1,42 @@
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import { Spinner } from "./components";
+import { Home } from "./pages";
+
+const lazyLoadedData = [
+  {
+    PageName: lazy(() => import("./pages/Project/Project")),
+    path: "/project",
+  },
+  {
+    PageName: lazy(() => import("./pages/NotFound/NotFound")),
+    path: "*",
+  },
+];
+
+const LazyLoadedPages = lazyLoadedData.map(({ PageName, path }) => {
+  const page = (
+    <Suspense
+      fallback={
+        <div>
+          <Spinner />
+        </div>
+      }
+    >
+      <PageName />
+    </Suspense>
+  );
+
+  return <Route element={page} path={path} />;
+});
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<Home />} path="/" />
+      {LazyLoadedPages}
+    </Routes>
+  );
+};
+
+export default AppRoutes;
